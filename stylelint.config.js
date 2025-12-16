6fbb317ace594e499f9a5321f7cabfe2b21b4962
Dmitry Yatskevich <dima.ja06@gmail.com>
2025-12-16 16:03:38 +0300

    feat: add Stylelint configuration for consistent styling and husky for git-hooks

---
+++ stylelint.config.js
@@ -0,0 +1,6 @@
+/** @type {import("stylelint").Config} */
+export default {
+  extends: ['stylelint-config-standard', 'stylelint-config-idiomatic-order'],
+  plugins: ['stylelint-order', 'stylelint-prettier'],
+  rules: { 'prettier/prettier': true },
+};
